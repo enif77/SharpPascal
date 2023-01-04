3190a65518fba83ce2a6fdf05c4b1ee79e1a9d08
enif77 <premysl.fara@gmail.com>
2023-01-04 16:14:15 +0100

    Program lists found tokens.

--- SharpPascal/Program.cs
+++ SharpPascal/Program.cs
@@ -21,17 +21,8 @@ namespace SharpPascal
 
             try
             {
-                var parser = new Parser(
-                new Tokenizer(
-                    new StringSourceReader(
-                        File.ReadAllText(args[0])
-                        )
-                    )
-                );
-
-                var p = parser.Parse();
-
-                File.WriteAllText(args[0] + ".cs", p.GenerateOutput());
+                TokenizeProgram(args[0]);
+                CompileProgram(args[0]);
             }
             catch (Exception ex)
             {
@@ -43,6 +34,56 @@ namespace SharpPascal
             Console.WriteLine("DONE");
 
             return 0;
+        }
+
+        
+        private static void TokenizeProgram(string path)
+        {
+            var tokenizer = new Tokenizer(
+                new StringSourceReader(
+                    File.ReadAllText(path)
+                )
+            );
+
+            var tok = tokenizer.NextToken();
+            while (tok.Code != TokenCode.TOK_EOF)
+            {
+                switch (tok.Code)
+                {
+                    case TokenCode.TOK_IDENTIFIER:
+                        Console.WriteLine("[{0:0000}:{1:0000}] identifier({2})", tok.Line, tok.LinePosition, tok);
+                        break;
+
+                    default:
+                        Console.WriteLine("[{0:0000}:{1:0000}] {2}", tok.Line, tok.LinePosition, tok);
+                        break;
+                }
+
+                tok = tokenizer.NextToken();
+            }
+            
+            Console.WriteLine("[{0:0000}:{1:0000}] {2}", tok.Line, tok.LinePosition, tok);
+            Console.WriteLine();
+        }
+        
+        
+        private static void CompileProgram(string path)
+        {
+            Console.WriteLine("Compiling: {0}", path);
+            
+            var parser = new Parser(
+                new Tokenizer(
+                    new StringSourceReader(
+                        File.ReadAllText(path)
+                    )
+                )
+            );
+
+            var p = parser.Parse();
+
+            File.WriteAllText(path + ".cs", p.GenerateOutput());
+            
+            Console.WriteLine("Compilation OK.");
         }
     }
 }
